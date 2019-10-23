3fbc812a59972a7db6c91045b31e5b1882f1cf37
TimMarotta <timothy.marotta@yahoo.com>
2019-10-23 10:42:10 -0400

    Started setup of all_players.py

---
+++ player4/all_players.py
@@ -0,0 +1,48 @@
+def get_default_player(symbol):
+    """
+    :returns: a default minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_a(symbol):
+    """
+    :author: Josh Hayden
+    :enchancement: Advanced Evaluation Functions
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_b(symbol):
+    """
+    :author: Tea Mdevadze
+    :enchancement:
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_c(symbol):
+    """
+    :author: Timothy Marotta
+    :enchancement: Transposition Table
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_d(symbol):
+    """
+    :author: John Hunter
+    :enchancement: ProbCut (TBD)
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_combined_player(symbol):
+    """
+    :returns: the best combination of the minimax enhancements that your team can create
+    """
+    pass
